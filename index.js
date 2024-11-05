5e72fb67306447161c778c36fb9ffd5cf540d54e
halazYoha <hailemariamyohannes07@gmail.com>
2024-11-05 10:31:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const decbtn=document.getElementById("decbtn");
+const resetbtn=document.getElementById("resetbtn");
+const incbtn=document.getElementById("incbtn");
+let count=0;
+incbtn.onclick=function(){
+    count++;
+    document.getElementById("countlabel").textContent=count;
+
+}
+window.alert('why');
+
+
